5a36da45bd62f6191fbb5b32444545c14339ae5a
Tarasov_EO <trsv.egor@gmail.com>
2021-11-01 00:41:09 +0300

    домашнее задание 4.4 упражнение 3

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello again!");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter FIO: ");
+//        String fullName = scanner.next();
+
+        String fullName = "Tarasov Egor Olegovich";
+
+        String familia = fullName.substring(0, fullName.indexOf(" "));
+        String name = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
+        String otchestvo = fullName.substring(fullName.lastIndexOf(" ") + 1, fullName.length());
+
+        System.out.println("Фамилия: " + familia);
+        System.out.println("Имя: " + name);
+        System.out.println("Отчество: " + otchestvo);
+
     }
 }
