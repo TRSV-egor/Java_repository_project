eaee24aa025ef4e4b43f29e0332a3928be4d0dd5
Tarasov_EO <trsv.egor@gmail.com>
2021-11-04 22:01:36 +0300

    ДЗ 4.5 задание 3

--- src/main.java
+++ src/main.java
@@ -2,7 +2,26 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+class main {
+    public static void main(String[] args) {
 
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ФИО:");
+        String line = scanner.nextLine();
+
+        String massive[] = line.split("\\s");
+
+        String firstName = massive[0];
+        String lastName = massive [1];
+        String patronymic = massive [2];
+
+        System.out.println("Фамилия " + firstName + "\nИмя " + lastName + "\nОтчество " + patronymic);
+
+
+
+    }
+}
 
 
 
