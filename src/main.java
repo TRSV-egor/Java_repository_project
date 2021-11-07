ef81324ad482034f65dabfe5950790d98c7ca64f
Tarasov_EO <trsv.egor@gmail.com>
2021-11-07 14:43:19 +0300

    ДЗ 4.5 задание со звёздочкой

--- src/main.java
+++ src/main.java
@@ -3,12 +3,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-
-class main{
-    public static void main(String[] args) {
-        
-    }
-}
+//ДЗ 4.5 задание со звёздочкой
+// public class main {
+//        public static void main(String[] args) {
+//            String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
+//            System.out.println(safe);
+//        }
+    
+//        public static String searchAndReplaceDiamonds(String text, String placeholder){
+//            String safe = text.replaceAll("<\\d+ \\d+ \\d+>", placeholder);
+//            return safe;
+//        }
+//     }
 
 //ДЗ 4.5 задание 4
 // class main{
